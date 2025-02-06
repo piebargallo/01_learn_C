e31439cb15bad726ede915edcdb543fd97aec825
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-02-06 17:34:43 +0100

    Add files via upload

---
+++ 03_tipus_enum/03_tipus_enum.c
@@ -0,0 +1,14 @@
+/* algorisme:
+	type
+		typeName = {VALUE1, VALUE2, VALUE3, ... , VALUEN}
+	end type */
+
+//typedef enum { VALUE1, VALUE2, VALUE3, ..., VALUEN } typeName
+
+typedef enum {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY } daysOfTheWeek;
+
+int main()
+{
+	daysOfTheWeek firstDay = MONDAY;
+	daysOfTheWeek lastDay = SUNDAY;
+}
