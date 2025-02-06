39851095e15d823a3a61735f576cf17185b9fba0
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-02-06 13:53:12 +0100

    Add files via upload

---
+++ 01_def_const/01_implementar_const.c
@@ -0,0 +1,25 @@
+/* Algorisme:
+	const
+		CONST_NAME: type = value;
+	end const */
+
+/* program presentació */
+#define CONST_NAME value 
+
+/* Algorisme:
+const
+	PI: real = 3.1415296;
+	MAX_STUDENTS: = integer = 50;
+	IVA: integer = 21;
+	BLANK_CHAR: char = '';
+end const */
+
+#define PI 3.1415926
+#define MAX_STUDENTS 50
+#define IVA 21
+#define BLANK_CHAR ' '
+
+int main() {
+	return 0;
+}
+
