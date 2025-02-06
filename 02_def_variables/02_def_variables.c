cda9ee52eba0c1596a3afdd1bad4d18e301cf461
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-02-06 17:34:23 +0100

    Add files via upload

---
+++ 02_def_variables/02_def_variables.c
@@ -0,0 +1,35 @@
+/* algorisme:
+	var
+		variableName: type;
+	end var
+		variableName := value;
+
+	var
+		variableName1, variableName2, variableName3: type;
+	end var
+
+	var
+		roomLength: real;
+		roomWidth: real;
+		currentNumberStudents: integer;
+		initialLetter: char;
+	end var
+		roomLength:= 0.0;
+		roomWidth:= 0.0;
+		currentNumberStudents:= 0;
+		initialLetter:= ' ';
+	*/
+
+int main() 
+{
+	float roomLength;
+	float roomWidth;
+	int curentNumberStudents;
+	char initialLetter;
+
+	roomLength = 0.0;
+	roomWidth = 0.0;
+	curentNumberStudents = 0;
+	initialLetter = ' ';
+
+}
